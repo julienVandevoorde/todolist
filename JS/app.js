af1f276e49928e69b5c5face5a6fcfcf52727ebe
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2023-03-23 12:21:52 +0100

    Version 0.1 non fonctionnelle

--- JS/app.js
+++ JS/app.js
@@ -3,3 +3,21 @@
 const inputTask = document.forms['frmTODO'].task;
 const btAdd = document.forms['frmTODO'].btAdd;
 const taskList = document.getElementById['taskList'];
+const divNotification = document.getElementById['notification'];
+
+// traitement des actions
+btAdd.onclick = function(){
+    //lecture des données
+    let task = inputTask.value;
+
+    //validation des données
+    if(task!='' && task.length>=3){
+        divNotification.innerHTML = "";
+        
+        //traitement des données
+        taskList.innerHTML += '<li>'+task+'</li>';
+
+    } else {
+        divNotification.innerHTML = 'Veuillez entrer un tache d\'au moins 3 caractères';
+    }
+}
