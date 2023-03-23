2be3eb9c203301a8626ca0e498ff1e942708d2e8
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2023-03-23 11:57:18 +0100

    Ajout du script JS

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+/* JS */
+// sélzction des éléments du DOM HTML
+const inputTask = document.forms['frmTODO'].task;
+const btAdd = document.forms['frmTODO'].btAdd;
+const taskList = document.getElementById['taskList'];
